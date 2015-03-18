c391b684aebaa3cb22ef1717d6305ba4bf71ec94
claire <jis550@newschool.edu>
2015-03-18 00:10:25 -0400

    adding a file

---
+++ javascript.js
@@ -0,0 +1,21 @@
+var name = prompt ("what's your name?");
+
+
+var xholder = document.getElementById("xholder");
+xholder.innerHTML = name;
+
+
+
+
+
+var color = prompt ("what's your favorite color?");
+
+var xholder = document.getElementById("xholder2");
+xholder.innerHTML = color;
+
+
+var birthday = prompt ("when is your birthday?");
+
+var xholder = document.getElementById("xholder3");
+xholder.innerHTML = birthday;
+
